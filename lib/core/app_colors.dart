b53d41e97ea789301fdaf9578fc5cb14348d8080
César Augusto José <cesar_augusto970@hotmail.com>
2026-02-05 13:13:11 -0300

    Altera chartPrimary para tom de rosa

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -8,7 +8,7 @@ class AppColors {
   static final Color lightGrey = Color(0xFFA6A1B2);
   static final Color border = Color(0xFFE1E1E6);
   static final Color chartSecondary = Color(0xFFE1E6E3);
-  static final Color chartPrimary = darkGreen;
+  static final Color chartPrimary = Color(0xFFE83D8E);
 
   //Greens
   static final Color lightGreen = Color(0xFFE1F5EC);
